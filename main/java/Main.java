c9f347f7e4b3d137f1af24ffd60bf5edcba1837c
Zahra <zahram1087@gmail.com>
2018-11-15 12:57:54 -0800

    firstproject

---
+++ main/java/Main.java
@@ -0,0 +1,38 @@
+import javax.security.sasl.SaslServer;
+import java.awt.*;
+
+public class Main {
+
+    public static void main(String [] args){
+        //write your code here
+        // \n -new line
+        // \t - new tab
+        // \b -backspace (removes the character before it)
+//        System.out.println("-Mike \n\t > Semi-skimmed \n\t > Medium\n\t - Eggs \n\t > Free-range \n\t - Bread");
+problem3();
+
+
+//        System.out.println("Hello World");
+//        System.out.println("Cat");
+//        System.out.println("Dog");
+//        System.out.println("Rabbit");
+//        simba();
+//      System.out.println("The dog said \"woolf\", then rolled around");
+
+    }
+
+    public static void problem3(){
+
+        System.out.println("The");
+        System.out.println("\t Cake");
+        System.out.print("Is");
+        System.out.println("\t A");
+        System.out.println("Lie");
+//        The
+//                Cake
+//        Is        A
+//        Lie.
+
+    }
+
+}
